415e4c899e78c0e547091d3b9bcaccded4f48b36
Israel <akinboyewaiyanuoluwa15@gmail.com>
2024-09-09 11:13:31 +0100

    Changed the look of the loading-feature

--- Frontend/src/components/spinner.jsx
+++ Frontend/src/components/spinner.jsx
@@ -5,10 +5,10 @@ export const Spinner = () => {
     <>
       <div className="grid grid-cols-2">
         <div
-          className="animate-spin w-[200px] h-[200px] ml-[620px] mt-[200px] bg-gray-700 rounded-3xl justify-center grid-cols-1"
+          className="animate-spin w-[200px] h-[200px] ml-[620px] mt-[200px] bg-gray-700 rounded-3xl justify-center"
           viewbox="0 0 0 0"
         ></div>
-        <span className="text-3xl ml-[800px] mb-[300px]">Loading...</span>
+        <span className="text-3xl ml-[820px] mb-[500px]">Loading...</span>
       </div>
     </>
   );
